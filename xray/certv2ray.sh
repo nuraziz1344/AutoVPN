09a202804553fa77650c252b9408caf484dd4d14
Caliph Dev <92326690+caliphdev@users.noreply.github.com>
2023-12-13 14:12:31 +0700

    Update certv2ray.sh

--- xray/certv2ray.sh
+++ xray/certv2ray.sh
@@ -21,10 +21,11 @@ sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
 echo -e "${CYAN}Updating packages...${NC}"
 sudo apt-get update
 echo -e "${CYAN}Installing Certbot...${NC}"
-sudo apt-get install certbot python3-certbot-nginx
+sudo apt-get install certbot python3-certbot-nginx -y
 echo -e "${CYAN}Obtaining SSL certificate...${NC}"
 sudo certbot certonly --standalone -d $domain --pre-hook "systemctl stop xray" --post-hook "systemctl start xray" --register-unsafely-without-email
 echo -e "${CYAN}Creating symbolic links...${NC}"
 sudo ln -sf /etc/letsencrypt/live/$domain/fullchain.pem /etc/xray/xray.crt
 sudo ln -sf /etc/letsencrypt/live/$domain/privkey.pem /etc/xray/xray.key
+systemctl restart nginx
 echo -e "${GREEN}SSL certificate obtained and configured successfully.${NC}"
